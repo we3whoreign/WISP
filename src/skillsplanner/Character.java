c94647fe511da55e8fae2fac52aa0690e3cdce12
ryzngard <ryzngard@gmail.com>
2011-07-27 18:48:10 -0400

    Relocated Character.java

---
+++ src/skillsplanner/Character.java
@@ -0,0 +1,27 @@
+package skillsplanner;
+
+import skillsplanner.classes.DFOClass;
+/**
+* Represents the character object that encapsulates what the player would be. 
+* Includes a class and stats, as well as skills and sp.
+*/
+public class Character{
+	private int remainingSP;
+	
+	//CHANGE LATER
+	private final int totalSP = 500;
+	
+	private DFOClass charclass;
+	
+	public Character(){
+		this.charclass = null;
+	}
+	
+	public void setDFOClass(DFOClass charclass){
+		this.charclass = charclass;
+	}
+	
+	public DFOClass getDFOClass(){
+		return this.charclass;
+	}
+}
