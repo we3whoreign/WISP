e91c661ec358e4a469a488ba587e91d2af7345f0
ryzngard <ryzngard@gmail.com>
2011-09-21 16:44:22 -0400

    added all the class xml

--- src/skillsplanner/gui/SamWise.java
+++ src/skillsplanner/gui/SamWise.java
@@ -9,9 +9,26 @@ import com.sun.corba.se.spi.orbutil.fsm.Input;
 
 public class SamWise implements ActionListener{
 
+	public static ActionEvent ClassSelection;
+	public static ActionEvent TabSelection;
+	public static ActionEvent SkillUp;
+	public static ActionEvent SkillDown;
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		if(ClassSelection.equals(e)){
+			
+		}
+		else if(TabSelection.equals(e)){
+			
+		}
+		else if(SkillUp.equals(e)){
+			
+		}
+		
+		else if(SkillUp.equals(e)){
+			
+		}
 
 	}
 
